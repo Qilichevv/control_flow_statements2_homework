0fb9af42f22f35be9fa4e3247b8164b6f92e1639
qilichevv <behruzqilichevv23@gmail.com>
2022-06-22 14:57:00 +0300

    if06

--- if06.py
+++ if06.py
@@ -6,4 +6,31 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    s=0
+    ind=0
+    x1=n%10
+    n//=10
+    if(s<x1):
+        s=x1
+        ind=1
+    x2=n%10
+    n//=10
+    if(s<x2):
+        s=x2
+        ind=2
+    x3=n%10
+    n//=10
+    if(s<x3):
+        s=x3
+        ind=3
+    x4=n%10
+    n//=10
+    if(s<x4):
+        s=x4
+        ind=4
+    x5=n%10
+    n//=10
+    if(s<x5):
+        s=x5
+        ind=5
+    return ind
