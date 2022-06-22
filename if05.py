83febbaf0e93b9f8ce5cc55e53bc6140d510eb60
qilichevv <behruzqilichevv23@gmail.com>
2022-06-22 14:40:34 +0300

    if05

--- if05.py
+++ if05.py
@@ -7,19 +7,19 @@ def main(n):
         int: return answer.
     """
     x1=n%10
-    n//10
+    n//=10
 
     x2=n%10
-    n//10
+    n//=10
 
     x3=n%10
-    n//10
+    n//=10
 
     x4=n%10
-    n//10
+    n//=10
 
     x5=n%10
-    n//10
+    n//=10
     if x1>=x2 and x1>=x3 and x1>=x4 and x1>=x5:
         return x1
     if x2>=x1 and x2>=x3 and x2>=x4 and x2>=x5:
