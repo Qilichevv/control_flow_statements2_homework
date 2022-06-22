27a11f480e11e1dc41dc9af95a5c333827b885ca
qilichevv <behruzqilichevv23@gmail.com>
2022-06-22 13:41:28 +0300

    if3

--- if03.py
+++ if03.py
@@ -8,10 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    if (a<=b and a>=c) :
+    if (a<=b and a>=c) or (a>=b and a<=c):
         return a
-    if b<=a and b>=c:
+    if (b<=a and b>=c) or (b>=a and b<=c):
         return b
-    if c<=a and c>=b:
-        return c
+    
+    return c
      
